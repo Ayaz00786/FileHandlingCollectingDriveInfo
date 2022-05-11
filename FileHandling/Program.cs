7b157a286f704a55531023c4cb6489738ef80592
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-11 11:43:35 +0530

    Add project files.

---
+++ FileHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //------Collecting Drive Info----------
+
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+
+            foreach (var drive in driveInfos)
+            {
+                Console.WriteLine( "-----------------------------");
+                Console.WriteLine("Drive Name:" +drive.Name);
+                Console.WriteLine("Total Size:"+ drive.TotalSize);
+                Console.WriteLine("Avilable Free Size:" + drive.AvailableFreeSpace);
+                Console.WriteLine("Drive Condition :" + drive.IsReady);
+                Console.WriteLine("Drive Type:" + drive.DriveType);
+            }
+        }
+    }
+}
